84d39a1c75863c2dddc9d510242d1db1f1421a08
AKR9634 <akhil6july2003@gmail.com>
2026-04-18 11:16:10 +0530

    Persistence Completed!!!

---
+++ Basic_ChatBot_Using_Persistence.py
@@ -0,0 +1,58 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, List, Annotated
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+
+# Defining the state class
+class chat_state(TypedDict):
+
+    message : Annotated[List[BaseMessage], add_messages]
+
+
+# Defining the Chat node
+def Chat(state: chat_state) -> chat_state:
+
+    result = model.invoke(state['message'])
+
+    return {'message': [result]}
+
+
+# Creating Graph
+
+graph = StateGraph(chat_state)
+
+graph.add_node('chat_node', Chat)
+
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+checkpointer = MemorySaver()
+
+workflow = graph.compile(checkpointer=checkpointer)
+
+config = {"configurable" : {"thread_id" : "1"}}
+
+while True:
+
+    user_input = input("User: ")
+
+    if user_input.lower().strip() in ['exit', 'bye', 'okkk']:
+        break
+
+    result = workflow.invoke({'message': HumanMessage(content=user_input)}, config=config)
+
+    print("AI: ", result['message'][-1].content)
+
